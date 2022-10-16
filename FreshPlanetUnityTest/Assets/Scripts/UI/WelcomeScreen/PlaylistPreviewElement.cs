7d9b502b97c60356dffea290d6dac4ddd3b11fd9
Jane Glazycheva <glazychevaeo@gmail.com>
2022-10-15 20:14:03 -0400

    Implemented Welcome screen functionality
    - Load/unload of playlist preview elements by status
    - Playlist progress using PlayerPrefs
    - Section select
    - Implemented utility class Singleton
    - Updated scene and UI prefabs

---
+++ FreshPlanetUnityTest/Assets/Scripts/UI/WelcomeScreen/PlaylistPreviewElement.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using FreshPlanet.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FreshPlanet.UI.WelcomeScreen
+{
+    [RequireComponent(typeof(Button))]
+    public class PlaylistPreviewElement : MonoBehaviour
+    {
+        public static event Action<PlaylistPreviewElement, Playlist> OnPlaylistSelected;
+        
+        [SerializeField]
+        private Button playlistButton;
+        [SerializeField]
+        private TextMeshProUGUI playlistLabel;
+        [SerializeField]
+        private CanvasGroup progressCanvasGroup;
+        [SerializeField]
+        private List<Image> progressImages;
+        [SerializeField]
+        private Sprite inProgressIcon;
+        [SerializeField]
+        private Sprite completedIcon;
+        [SerializeField]
+        private CanvasGroup masteredCanvasGroup;
+
+        private Playlist playlistData;
+
+        private void Awake()
+        {
+            playlistButton.onClick.AddListener(HandlePlaylistButtonClicked);
+        }
+
+        private void OnDestroy()
+        {
+            playlistButton.onClick.RemoveListener(HandlePlaylistButtonClicked);
+        }
+
+        public void DisplayPlaylist(Playlist playlist)
+        {
+            playlistData = playlist;
+            playlistLabel.text = playlistData.PlaylistTitle;
+            bool mastered = playlistData.GetPlaylistStatus() is PlaylistStatus.Mastered;
+
+            progressCanvasGroup.alpha = mastered ? 0 : 1;
+            masteredCanvasGroup.alpha = mastered ? 1 : 0;
+
+            if (!mastered)
+            {
+                int completedAmount = playlistData.GetMaxCompletedQuestions();
+                
+                for (int i = 0; i < progressImages.Count; i++)
+                {
+                    progressImages[i].sprite = i < completedAmount? completedIcon : inProgressIcon;
+                }
+            }
+        }
+
+        private void HandlePlaylistButtonClicked()
+        {
+            OnPlaylistSelected?.Invoke(this, playlistData);
+        }
+    }
+}
---
+++ FreshPlanetUnityTest/Assets/Scripts/UI/WelcomeScreen/WelcomeUIScreen.cs
@@ -0,0 +1,218 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using FreshPlanet.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FreshPlanet.UI.WelcomeScreen
+{
+    public class WelcomeUIScreen : UIScreen
+    {
+        [Header("Sections")]
+        [SerializeField]
+        private RectTransform sectionSelectArrow;
+        [SerializeField]
+        private RectTransform topPanelRectTransform;
+        [SerializeField]
+        private float sectionTransitionDuration = 0.5f;
+        
+        [Serializable]
+        public class SectionGroup
+        {
+            public PlaylistStatus status;
+            public TextMeshProUGUI sectionLabel;
+            public RectTransform rectTransform;
+            public Button sectionButton;
+        }
+
+        [SerializeField]
+        private List<SectionGroup> sectionGroups = new List<SectionGroup>();
+
+        [Header("Playlists")]
+        [SerializeField]
+        private RectTransform playlistsContentParent;
+        [SerializeField]
+        private CanvasGroup playlistContentCanvasGroup;
+        [SerializeField]
+        private PlaylistPreviewElement playlistPreviewElement;
+        
+        private readonly List<PlaylistPreviewElement> spawnedPlaylistElements = new List<PlaylistPreviewElement>();
+        private readonly Dictionary<PlaylistStatus, SectionGroup> statusSectionTable =
+            new Dictionary<PlaylistStatus, SectionGroup>();
+        private readonly Dictionary<PlaylistStatus, List<Playlist>> playlistStatusTable =
+            new Dictionary<PlaylistStatus, List<Playlist>>();
+        private SectionGroup selectedSection;
+        private Tween arrowTween;
+        private Tween fadeTween;
+        private Coroutine sectionChangeRoutine;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            PlaylistPreviewElement.OnPlaylistSelected += HandlePlaylistSelected;
+            
+            foreach (SectionGroup sectionGroup in sectionGroups)
+            {
+                if (statusSectionTable.ContainsKey(sectionGroup.status))
+                {
+                    continue;
+                }
+                
+                statusSectionTable.Add(sectionGroup.status, sectionGroup);
+                sectionGroup.sectionButton.onClick.AddListener(() => HandleStatusSectionClicked(sectionGroup.status));
+            }
+        }
+
+        private void OnDestroy()
+        {
+            PlaylistPreviewElement.OnPlaylistSelected -= HandlePlaylistSelected;
+            
+            foreach (SectionGroup sectionGroup in statusSectionTable.Values)
+            {
+                sectionGroup.sectionButton.onClick.RemoveListener(() => HandleStatusSectionClicked(sectionGroup.status));
+            }
+
+            TerminateRoutine();
+        }
+
+        private void TerminateRoutine()
+        {
+            arrowTween?.Kill();
+            fadeTween?.Kill();
+            
+            if (sectionChangeRoutine != null)
+            {
+                StopCoroutine(sectionChangeRoutine);
+                sectionChangeRoutine = null;
+            }
+        }
+
+        protected override IEnumerator TransitionIn()
+        {
+            LoadPlaylists();
+            SelectSection(PlaylistStatus.Activated, true);
+            yield return base.TransitionIn();
+        }
+
+        protected override IEnumerator TransitionOut()
+        {
+            yield return base.TransitionOut();
+            TerminateRoutine();
+        }
+
+        private PlaylistPreviewElement SpawnPlaylistPreviewElement()
+        {
+            PlaylistPreviewElement element = Instantiate(playlistPreviewElement, playlistsContentParent);
+            spawnedPlaylistElements.Add(element);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(playlistsContentParent);
+            return element;
+        }
+
+        private void LoadPlaylists()
+        {
+            playlistStatusTable.Clear();
+           
+            foreach (Playlist playlist in QuizPlaylists.Playlists)
+            {
+                PlaylistStatus status = playlist.GetPlaylistStatus();
+
+                if (playlistStatusTable.TryGetValue(status, out List<Playlist> playlists))
+                {
+                    playlists.Add(playlist);
+                }
+                else
+                {
+                    playlistStatusTable.Add(status, new List<Playlist>(){ playlist });
+                }
+            }
+        }
+
+        private void SelectSection(PlaylistStatus status, bool instant = false)
+        {
+            SectionGroup sectionGroup = statusSectionTable[status];
+            if (selectedSection == sectionGroup)
+            {
+                return;
+            }
+
+            if (selectedSection != null)
+            {
+                bool isSet = (selectedSection.sectionLabel.fontStyle & FontStyles.Bold) != 0;
+                if(isSet)
+                {
+                    selectedSection.sectionLabel.fontStyle ^= FontStyles.Bold;
+                }
+            }
+            
+            selectedSection = sectionGroup;
+            selectedSection.sectionLabel.fontStyle |= FontStyles.Bold;
+            
+            TerminateRoutine();
+            sectionChangeRoutine = StartCoroutine(SectionChangeRoutine(instant));
+        }
+
+        private IEnumerator SectionChangeRoutine(bool instant)
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(topPanelRectTransform);
+
+            arrowTween?.Kill();
+            arrowTween = sectionSelectArrow.DOMoveX(selectedSection.rectTransform.position.x, instant ? 0 : sectionTransitionDuration )
+                .SetEase(Ease.Linear);
+            fadeTween?.Kill();
+            
+            if (!instant)
+            {
+                playlistContentCanvasGroup.interactable = false;
+                fadeTween = playlistContentCanvasGroup.DOFade(0, sectionTransitionDuration / 2)
+                    .SetEase(Ease.Linear);
+                yield return fadeTween.WaitForCompletion();
+            }
+            
+            DisplayPlaylists();
+
+            fadeTween = playlistContentCanvasGroup.DOFade(1, instant ? 0 : sectionTransitionDuration / 2)
+                .SetEase(Ease.Linear);
+            yield return fadeTween.WaitForCompletion();
+            
+            playlistContentCanvasGroup.interactable = true;
+        }
+        
+        private void DisplayPlaylists()
+        {
+            playlistStatusTable.TryGetValue(selectedSection.status, out List<Playlist> playlists);
+            playlists ??= new List<Playlist>();
+            
+            if (spawnedPlaylistElements.Count > playlists.Count)
+            {
+                for (int i = spawnedPlaylistElements.Count - 1; i >= playlists.Count; i--)
+                {
+                    PlaylistPreviewElement element = spawnedPlaylistElements[i];
+                    DestroyImmediate(element.gameObject);
+                    spawnedPlaylistElements.Remove(element);
+                }
+                
+                LayoutRebuilder.ForceRebuildLayoutImmediate(playlistsContentParent);
+            }
+
+            for (int i = 0; i < playlists.Count; i++)
+            {
+                PlaylistPreviewElement element = spawnedPlaylistElements.Count > i ? spawnedPlaylistElements[i] : SpawnPlaylistPreviewElement();
+                element.DisplayPlaylist(playlists[i]);
+            }
+        }
+        
+        private void HandlePlaylistSelected(PlaylistPreviewElement previewElement, Playlist playlist)
+        {
+            PlaylistPreloader.Instance.PreloadPlaylist(playlist);
+            UIController.Instance.QuizScreen.Active = true;
+        }
+
+        private void HandleStatusSectionClicked(PlaylistStatus status)
+        {
+            SelectSection(status);
+        }
+    }
+}
